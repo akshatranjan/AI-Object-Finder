a90a1268aeaa0f27c04c44d13443aa397e7fe1f5
akshatranjan <73059364+akshatranjan@users.noreply.github.com>
2022-08-11 20:12:27 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 status = "";
 object = "";
+objects = [];
 function setup()
 {
     canvas = createCanvas(480, 380);
@@ -29,4 +30,39 @@ function modelLoaded()
 function draw()
 {
     image(video, 0, 0, 480, 380);
+    if (status != "")
+    {
+        objectDetector.detect(video, gotResults);
+        for (i=0; i<objects.length; i++)
+        {
+            percent = floor(objects[i].confidence * 100);
+            fill('#FF0000');
+            text(objects[i].label + " " + percent + "%", objects[i].x + 15, objects[i].y + 15);
+            noFill();
+            stroke('#FF0000');
+            rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
+
+            if (object == objects[i].label)
+    {
+        document.getElementById("status").innerHTML = "Object Found";
+        synth = window.speechSynthesis;
+        utterThis = new SpeechSynthesisUtterance(object + "Found"); synth.speak(utterThis);
+    }
+
+    else
+    {
+        document.getElementById("status").innerHTML = "Object Not Found";
+    }
+        }
+    }
+}
+
+function gotResults(error, results)
+{
+    if (error)
+    {
+        console.log(error);
+    }
+    console.log(results);
+    objects = results;
 }
